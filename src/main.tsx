e5989aa6c03075a61d5dbc4ba272c81bb0c33bc1
matheusrpd <matheusrpd@gmail.com>
2022-01-29 20:56:58 -0300

    Configurando fonts localmente.

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { Dashboard } from './App';
+import { Dashboard } from './Dashboard';
 
 ReactDOM.render(
   <React.StrictMode>
